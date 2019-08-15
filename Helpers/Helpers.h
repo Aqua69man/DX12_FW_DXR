bf3a45496e47d56634ef0a7536042e3de9f5c075
Farid Rzaev <farik69rzaev@gmail.com>
2019-08-15 23:01:48 +0300

    Adding MessageBox Helper func

--- Helpers/Helpers.h
+++ Helpers/Helpers.h
@@ -4,6 +4,7 @@
 #include <Windows.h> // For HRESULT
 #include <exception> // For std::exception
 #include <string>    // For msgBox
+#include <codecvt>   // For wstring_convert
 
 // From DXSampleHelper.h 
 // Source: https://github.com/Microsoft/DirectX-Graphics-Samples
@@ -15,7 +16,21 @@ inline void ThrowIfFailed(HRESULT hr)
 	}
 }
 
-inline void msgBox(const std::string& msg)
+inline std::wstring string_2_wstring(const std::string& s)
+{
+	std::wstring_convert<std::codecvt_utf8<WCHAR>> cvt;
+	std::wstring ws = cvt.from_bytes(s);
+	return ws;
+}
+
+inline std::string wstring_2_string(const std::wstring& ws)
+{
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> cvt;
+	std::string s = cvt.to_bytes(ws);
+	return s;
+}
+
+inline void MsgBox(const std::string& msg)
 {
 	HWND gWinHandle = nullptr;
 	MessageBoxA(gWinHandle, msg.c_str(), "Error", MB_OK);
--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdL
 	const wchar_t* windowTitle = L"Learning DirectX 12";
 
 	DxrGame game (hInstance, windowTitle, 3500, 1800, false);
+	game.InitDXR();
 	game.LoadContent(exePath);
 	game.Run();
 	game.UnloadContent();
