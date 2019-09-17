a5442724c0f62a9a1806433863b73958c5167312
Farid Rzaev <farik69rzaev@gmail.com>
2019-09-17 15:34:36 +0300

    dummpy refctor

---
+++ 2_RT_TrianglesRefit/main_DxrGame.cpp
@@ -0,0 +1,15 @@
+#include "DxrGame.h"
+
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
+{
+	std::wstring exePath = GetExePath();
+	assert(exePath.size() != 0);
+
+	const wchar_t* windowTitle = L"Learning DirectX 12";
+
+	DxrGame game (hInstance, windowTitle, 3500, 1800, false);
+	game.InitDXR();
+	game.Run();
+
+	return 0;
+}
